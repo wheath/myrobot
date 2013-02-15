63fd25f181a762296e9efa14dcf39b2f09e5b5b5
wheath <wgheath@gmail.com>
2013-02-15 13:22:51 -1000

    testing moving js dir

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 //$(document).bind('deviceready', function(){
 function onDeviceReady() {
     //$(function(){
-       alert('MyRobot app started');
-       $('#logon_button').click(function(){ alert('logon button clicked'); });
+       alert('MyRobot app started with moved js dir');
+       $('#logon_button').click(function(){ alert('logon button clicked with js dir moved'); });
     //});
 //});
 }
