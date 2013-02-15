1035e122c843dbe884ae4e27d8ca3697a2342205
wheath <wgheath@gmail.com>
2013-02-15 13:20:28 -1000

    experiment to see if moving js files to js will work

---
+++ js/app.js
@@ -0,0 +1,8 @@
+//$(document).bind('deviceready', function(){
+function onDeviceReady() {
+    //$(function(){
+       alert('MyRobot app started');
+       $('#logon_button').click(function(){ alert('logon button clicked'); });
+    //});
+//});
+}
