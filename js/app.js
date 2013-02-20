c6aff9a3c935c181f89e547e061f5ba567dea7e1
wheath <wgheath@gmail.com>
2013-02-20 00:15:34 -1000

    drupalgap testing

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 function onDeviceReady() {
   console.log('MyRobot app started with moved js dir');
   $('#logon_button').click(function() { 
-    console.log('logon button clicked with js dir moved'); 
+    alert('logon button clicked with js dir moved'); 
     try {
     $.ajax({
       url: "http://www.myrobot.com/drupal/drupalgap/system/connect.json",
@@ -15,12 +15,15 @@ function onDeviceReady() {
         console.log(JSON.stringify(errorThrown));
       },
       success: function (data) {
+        alert("success");
         var drupal_user = data.user;
         if (drupal_user.uid == 0) { // user is not logged in, show the login button, hide the logout button
+          alert("not logged in");
           $('#logon_button').show();
           $('#logout_button').hide();
         }
         else { // user is logged in, hide the login button, show the logout button
+          alert("logged in");
           $('#logon_button').hide();
           $('#logout_button').show();
         }
