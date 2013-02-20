d64ad16ed1063afd70cb4211e5178b76e074a5ae
wheath <wgheath@gmail.com>
2013-02-19 23:38:34 -1000

    attempting drupalgap login

--- assets/js/app.js
+++
@@ -1,8 +0,0 @@
-//$(document).bind('deviceready', function(){
-function onDeviceReady() {
-    //$(function(){
-       alert('MyRobot app started');
-       $('#logon_button').click(function(){ alert('logon button clicked'); });
-    //});
-//});
-}
--- js/app.js
+++ js/app.js
@@ -1,8 +1,33 @@
 //$(document).bind('deviceready', function(){
 function onDeviceReady() {
-    //$(function(){
-       alert('MyRobot app started with moved js dir');
-       $('#logon_button').click(function(){ alert('logon button clicked with js dir moved'); });
-    //});
-//});
+  console.log('MyRobot app started with moved js dir');
+  $('#logon_button').click(function() { 
+    console.log('logon button clicked with js dir moved'); 
+    try {
+    $.ajax({
+      url: "http://www.myrobot.com/drupal/drupalgap/system/connect.json",
+      type: 'post',
+      dataType: 'json',
+      error: function (XMLHttpRequest, textStatus, errorThrown) {
+        alert('page_dashboard - failed to system connect');
+        console.log(JSON.stringify(XMLHttpRequest));
+        console.log(JSON.stringify(textStatus));
+        console.log(JSON.stringify(errorThrown));
+      },
+      success: function (data) {
+        var drupal_user = data.user;
+        if (drupal_user.uid == 0) { // user is not logged in, show the login button, hide the logout button
+          $('#logon_button').show();
+          $('#logout_button').hide();
+        }
+        else { // user is logged in, hide the login button, show the logout button
+          $('#logon_button').hide();
+          $('#logout_button').show();
+        }
+      }
+    });
+  }
+  catch (error) { 
+    alert("page_dashboard - " + error); }
+  }
 }
