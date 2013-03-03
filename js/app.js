53b1aafae31f39d6e9f8b4173743dcbc06416acb
wheath <wgheath@gmail.com>
2013-03-03 00:57:33 -1000

    added process cellbot commands functionaility

--- js/app.js
+++ js/app.js
@@ -1,3 +1,43 @@
+function change_cb_cmd_to_processed(cb_cmd_nid) {
+  alert('updating field_state field of cellbot command with nid: ' + cb_cmd_nid + ' with command value: ' + cb_cmds[cb_cmd_nid] + ' to processed');
+
+  var updateObj = {"type":"cellbot_command","field_state":{"und":"processed"}};
+  $.ajax({
+      url: "http://www.myrobot.com/drupal/rest/node/" + cb_cmd_nid,
+      type: 'PUT',
+      data: JSON.stringify(updateObj),
+      dataType: 'json',
+      contentType: 'application/json',
+      error: function(XMLHttpRequest, textStatus, errorThrown) {
+        alert('failed to updated cellbot command to processed');
+        console.log(JSON.stringify(XMLHttpRequest));
+        console.log(JSON.stringify(textStatus));
+        console.log(JSON.stringify(errorThrown));
+      },
+      success: function (data) {
+        alert("success updating node to processed");
+      }
+  });
+}
+
+function get_cellbot_cmds(cb_node_id, obj) {
+    var cb_nid_to_cb_cmd = {};
+   for(var i=0;i < obj.length;i++) {
+    
+    console.log("_dbg obj[i].nid: " +  obj[i].nid);
+    console.log("_dbg obj[i].title: " +  obj[i].title);
+    console.log("_dbg obj[i].field_cb_node_id['und'][0]['value']: " +  obj[i].field_cb_node_id['und'][0]['value']);
+    console.log("_dbg obj[i].body['und'][0]['value']: " +  obj[i].body['und'][0]['value']);
+       if(obj[i].field_cb_node_id['und'][0]['value'] == cb_node_id) {
+          cb_nid_to_cb_cmd[obj[i].nid] = obj[i].body['und'][0]['value'];
+       }
+    
+   } 
+ return cb_nid_to_cb_cmd;
+}
+
+
+
 function onDeviceReady() {
   console.log('MyRobot app started with moved js dir');
   $('#logon_button').click(function() { 
@@ -82,10 +122,11 @@ $('#page_node_create_submit').live('click',function(){
   // END: drupal services node create
   });
 
-  $('#test_get_node').live('click',function(){
+  $('#process_cmds').live('click',function(){
     jQuery.ajax({
-            url : "http://www.myrobot.com/drupal/rest/node/16.json",
+            url : "http://www.myrobot.com/drupal/rest/view/newcbcmds.json",
             type : 'get',
+            data: 'filters[cb_node_id]=16',
             dataType : 'json',
             error : function(data) {
                 //error code
@@ -93,7 +134,13 @@ $('#page_node_create_submit').live('click',function(){
                 },
             success : function(data) {
               //success code
-              alert('Cookie Accepted' + JSON.stringify(data));
+              alert('retrieved cellbot commands from newcbcmds view: ' + JSON.stringify(data));
+              var cb_cmds = get_cellbot_cmds(16, data); 
+              alert('cb_cmds for cellbot node id 16: ' + JSON.stringify(cb_cmds));
+              for(var cb_cmd_nid in cb_cmds) {
+                alert('processing cellbot command with nid: ' + cb_cmd_nid + ' with command vaue: ' + cb_cmds[cb_cmd_nid]);
+                change_cb_cmd_to_processed(cb_cmd_nid); 
+              }
             }
    });
   });
