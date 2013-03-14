61d1346c792202e6d854adddf9548ed3cf135a74
wheath <wgheath@gmail.com>
2013-03-13 21:56:55 -1000

    added serial output

--- python_svr/cbcmds_svr.py
+++ python_svr/cbcmds_svr.py
@@ -1,5 +1,8 @@
 import web
 import simplejson as json
+import serial
+
+ser = serial.Serial('/dev/tty.iap', 9600, timeout=1)
 
 urls = (
   '/', 'index'
@@ -11,36 +14,12 @@ class index:
     cbfn = i.callback
     web.header('Content-Type', 'application/javascript') 
     print "_dbg processing cellbot command %s\n" % i.cmd
-    ret_data = getattr(self, i.cmd)(i)
+    ser.write("%c\n" % input)
+    ret_data = {}
+    ret_data['status'] = 'success'
+    ret_data['message'] = 'cellbot executed command: %s' % i.cmd  
+    print ret_data['message'] + '\n'
     return "%s(%s)" % (cbfn, json.dumps(ret_data)) 
-
-  def left(self, i):
-    ret_data = {}
-    ret_data['status'] = 'success'
-    ret_data['message'] = 'cellbot executed command: %s' % i.cmd  
-    print ret_data['message'] + '\n'
-    return ret_data;
-
-  def right(self, i):
-    ret_data = {}
-    ret_data['status'] = 'success'
-    ret_data['message'] = 'cellbot executed command: %s' % i.cmd  
-    print ret_data['message'] + '\n'
-    return ret_data;
-
-  def backward(self, i):
-    ret_data = {}
-    ret_data['status'] = 'success'
-    ret_data['message'] = 'cellbot executed command: %s' % i.cmd  
-    print ret_data['message'] + '\n'
-    return ret_data;
-
-  def forward(self, i):
-    ret_data = {}
-    ret_data['status'] = 'success'
-    ret_data['message'] = 'cellbot executed command: %s' % i.cmd  
-    print ret_data['message'] + '\n'
-    return ret_data;
 
 if __name__ == "__main__": 
     print "_dbg starting python svr\n";
